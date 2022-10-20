851f16a00b0b5fc239f539f67ec47bb3f17cafda
Jemila Ibrahim <jemila.ibrahim@wemabank.com>
2022-10-20 12:40:33 +0100

    Js is cool

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let editButton;
+let deleteBtn;
+let form;
+let todoListWrapper;
+let todoInputField;
+
 function onEditTodo() {
     alert("i am editing");
   }
@@ -5,6 +11,36 @@ function onEditTodo() {
   function onDeleteTodo() {
     alert(" i am deleting");
   }
+
+  //
+function showTodoItem(todo) {
+  let li = document.createElement('li');
+  li.innerHTML = `      <div>
+  <input class="todo__checkbox" type="checkbox" />
+  <span class="todo__text"> ${todo} </span>
+  <div class="todo__actions--wrapper">
+    <button id="edit_btn" class="todo__actions edit__btn">
+      edit
+    </button>
+    <button id="delete_btn" class="todo__actions delete__btn">
+      delete
+    </button>
+  </div>
+</div>`
+
+li.classList.add("todo__section--todoItem")
+  
+
+todoListWrapper.appendChild(li);
+}
+
+function onTodoFormSubmit(event) {
+event.preventDefault();
+
+const todoValue = todoInputField.value;
+
+showTodoItem(todoValue);
+}
   
   function setup() {
     const editButton = document.getElementById("edit_btn");
