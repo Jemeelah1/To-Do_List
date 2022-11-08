183ff5edcbc6aefba66b1550d5b7c8f29f582904
Jemila Ibrahim <jemila.ibrahim@wemabank.com>
2022-11-08 13:42:55 +0100

    complete Js snippet

--- index.js
+++ index.js
@@ -1,55 +1,90 @@
-let editButton;
-let deleteBtn;
-let form;
-let todoListWrapper;
-let todoInputField;
+let todoForm = document.getElementById("form");
+let listOfTodo = document.querySelector("#todo__list");
+let inputField = document.querySelector("#todo__input");
 
-function onEditTodo() {
-    alert("i am editing");
-  }
+
+
+function deleteTodo(eventObject) {
+  const buttonClicked = eventObject.target;
+  const todoItem = buttonClicked.parentNode.parentNode.parentNode;
+  todoItem.remove();
+}
+
+
+ function editTodoText (eventObject){
   
-  function onDeleteTodo() {
-    alert(" i am deleting");
-  }
+  let editButtonClicked = eventObject.target
+let li = editButtonClicked.parentNode.parentNode.parentNode
+let deletebtn = li.childNodes[1].childNodes[5]
+let child = li.childNodes[1].childNodes[3].innerHTML
+inputField.value = child
+let newbtn = document.createElement("button")
+newbtn.innerHTML = `Done`
+newbtn.classList.add("done-btn")
+li.append(newbtn)
+editButtonClicked.disabled = true
+newbtn.addEventListener("click", doThis)
+function doThis(){
+let childsy = li.childNodes[1].childNodes[3]
+childsy.innerHTML = inputField.value
+console.log(childsy.innerHTML)
+inputField.value = ""
+editButtonClicked.disabled = false
+newbtn.remove()
 
-  //
-function showTodoItem(todo) {
-  let li = document.createElement('li');
-  li.innerHTML = `      <div>
-  <input class="todo__checkbox" type="checkbox" />
-  <span class="todo__text"> ${todo} </span>
-  <div class="todo__actions--wrapper">
-    <button id="edit_btn" class="todo__actions edit__btn">
-      edit
-    </button>
-    <button id="delete_btn" class="todo__actions delete__btn">
-      delete
-    </button>
+}
+}
+  
+function selectButtons(){
+  let collectButtons = document.querySelectorAll(".delete-btn")
+  let lastButton = collectButtons[collectButtons.length - 1]
+  
+  lastButton.addEventListener("click", deleteTodo)
+  
+  let collectEditButtons = document.querySelectorAll(".edit-btn")
+  let lastEditButton = collectEditButtons[collectButtons.length - 1]
+  
+  lastEditButton.addEventListener("click", editTodoText)
+  
+  
+  }
+// 
+
+function showTodoOnTheScreen(todo) {
+ 
+  const li = document.createElement("li");
+  li.classList.add("todo__section-todoItem");
+
+  li.innerHTML = ` <div>
+  <input type="checkbox" class="todo-checkbox">
+  <span class="todo-text"> ${todo} </span>
+  <div class="todo_actions-wrapper">
+      <button  class="todo_actions edit-btn" id="edit_btn">edit</button>
+      <button  class="todo_actions delete-btn" id="delete_btn">delete</button>
   </div>
-</div>`
+</div>`;
 
-li.classList.add("todo__section--todoItem")
-  
-
-todoListWrapper.appendChild(li);
+  listOfTodo.appendChild(li);
+  selectButtons();
 }
 
-function onTodoFormSubmit(event) {
-event.preventDefault();
-
-const todoValue = todoInputField.value;
-
-showTodoItem(todoValue);
+function todoFormSumbitHandler(eventObject) {
+  eventObject.preventDefault();
+  
+  const todoValue = inputField.value;
+  showTodoOnTheScreen(todoValue);
+  inputField.value = "";
 }
-  
-  function setup() {
-    const editButton = document.getElementById("edit_btn");
-    const deleteBtn = document.getElementById("delete_btn");
-  
-    editButton.addEventListener("click", onEditTodo);
-    deleteBtn.addEventListener("click", onDeleteTodo);
-  
-    console.log(editButton);
-  }
-  
-  setup();
+
+function clearForm() {
+
+  inputField.value = ""; 
+}
+
+
+
+
+todoForm.addEventListener("submit", todoFormSumbitHandler);
+
+const clearButton = document.querySelector("#Clear");
+clearButton.addEventListener("click", clearForm);
